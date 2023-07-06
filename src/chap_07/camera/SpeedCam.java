9aa18eb92adf19e582cd00789f4a94f4c95cc886
DESKTOP-4F871ER\이호승 <lhs199653@gmail.com>
2023-07-06 15:28:35 +0900

    상속

---
+++ src/chap_07/camera/SpeedCam.java
@@ -0,0 +1,16 @@
+package chap_07.camera;
+
+public class SpeedCam extends Camera{ // 자식 클래스
+
+    public SpeedCam() {
+        this.name = "과속 단속 카메라";
+    }
+
+    public void checkSpeed(){
+        System.out.println("속도를 측정합니다.");
+    }
+    public void recognizeLicensePlate(){
+        //번호 인식
+        System.out.println("차량 번호를 인식합니다.");
+    }
+}
