f201062c6aa3cda205786a00c07438bef7bdd383
DESKTOP-4F871ER\이호승 <lhs199653@gmail.com>
2023-07-06 12:48:43 +0900

    접근 제어자

---
+++ src/chap_08/_00_AccessModifierTest.java
@@ -0,0 +1,15 @@
+package chap_08;
+
+import chap_07.BlackBox;
+import chap_07.BlackBoxRefurbish;
+
+public class _00_AccessModifierTest {
+    public static void main(String[] args) {
+        BlackBoxRefurbish b1 = new BlackBoxRefurbish();
+        b1.modelName = "까망이";
+        //b1.getResolution() = "FHD"; //default
+        //b1.price = 10000; //private
+        //b1.color = "블랙"; //protected
+
+    }
+}
